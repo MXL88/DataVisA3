4a3d5071af5f72a728b0f4650c741a069ea6043e
Michelle Le <mxl@uw.edu>
2015-05-05 00:06:15 -0700

    got lines to show up

--- main.js
+++ main.js
@@ -8,10 +8,7 @@ var color = d3.scale.category10();
 
 
 
-var line = d3.svg.line()
-    .interpolate("basis")
-    .x(function(d) { return x(d.Year); })
-    .y(function(d) { return y(d.Count); });
+
 
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -20,6 +17,8 @@ var svg = d3.select("body").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.csv("names.csv", function(error, data) {
+  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "Year"; }));
+
   data.forEach(function(d) {
     Year = +d.Year;
     Name = d.Name;
@@ -32,8 +31,12 @@ d3.csv("names.csv", function(error, data) {
     .range([0, width]);
 
   var y = d3.scale.linear()
-    .domain([0, 35000])
     .range([height, 0]);
+
+  var line = d3.svg.line()
+  .interpolate("basis")
+  .x(function(d) { return x(d.Year); })
+  .y(function(d) { return y(d.Count); });
 
 
   var xAxis = d3.svg.axis()
@@ -48,13 +51,19 @@ d3.csv("names.csv", function(error, data) {
     return {
       name: name,
       values: data.map(function(d) {
-        return {Year: +[d.Year], Name: d.Name, Gender: d.Gender,
+        return {Year: [d.Year], Name: d.Name, Gender: d.Gender,
                 Count: +[d.Count], Ranking: +[d.Ranking]};
       })
     };
   });
 
   x.domain(d3.extent(data, function(d) { return d.Year; }));
+
+  y.domain([
+    d3.min(names, function(c) { return d3.min(c.values, function(v) { return v.Count; }); }),
+    d3.max(names, function(c) { return d3.max(c.values, function(v) { return v.Count; }); })
+  ]);
+
 
 
   svg.append("g")
@@ -67,14 +76,14 @@ d3.csv("names.csv", function(error, data) {
       .call(yAxis)
     .append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 35000)
+      .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Count");
+      .text("Count of Names");
 
   var name = svg.selectAll(".name")
-      .data(name)
-    .enter().append("g")
+      .data(names)
+      .enter().append("g")
       .attr("class", "name");
 
   name.append("path")
@@ -82,10 +91,9 @@ d3.csv("names.csv", function(error, data) {
       .attr("d", function(d) { return line(d.values); })
       .style("stroke", function(d) { return color(d.Name); });
 
-//   name.append("text")
-//       .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-//       .attr("transform", function(d) { return "translate(" + x(d.value.year) + "," + y(d.value.count) + ")"; })
-//       .attr("x", 2020)
-//       .attr("dy", ".35em")
-//       .text(function(d) { return d.name; });
+  name.append("text")
+      .datum(function(d) { return {name: d.Name, value: d.values[d.values.length - 1]}; })
+      .attr("transform", function(d) { return "translate(" + x(d.value.year) + "," + y(d.value.count) + ")"; })
+      .attr("dy", ".35em")
+      .text(function(d) { return d.Name; });
  });
